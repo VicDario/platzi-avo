03c4eceb3b2ad83579ba4eb8013090a0c67cd7ef
VicDario <26585356+VicDario@users.noreply.github.com>
2022-01-22 21:45:33 -0300

    [Change] Function name

--- pages/api/avo/[id].ts
+++ pages/api/avo/[id].ts
@@ -1,11 +1,11 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import DB from '@database';
 
-const allAvos = async(req: NextApiRequest, res: NextApiResponse) => {
+const avoById = async(req: NextApiRequest, res: NextApiResponse) => {
     const db = new DB();
     const { id } = req.query;
     const avo = await db.getById(id as string);
     res.status(200).json(avo);
 }
 
-export default allAvos;
+export default avoById;

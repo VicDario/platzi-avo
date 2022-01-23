ee9038400b9d67e23dfdffd7f47d21958b775922
VicDario <26585356+VicDario@users.noreply.github.com>
2022-01-22 21:40:15 -0300

    [Changes] Next dependences for endpoint

--- pages/api/avo/index.ts
+++ pages/api/avo/index.ts
@@ -1,20 +1,14 @@
-import { IncomingMessage, ServerResponse } from 'http';
+import { NextApiRequest, NextApiResponse } from 'next';
 import DB from '@database';
 
-const allAvos = async(req: IncomingMessage, res: ServerResponse) => {
+const allAvos = async(req: NextApiRequest, res: NextApiResponse) => {
     const db = new DB();
     const allEntries = await db.getAll();
     const length = allEntries.length;
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.end(
-        JSON.stringify(
-            {
-                data: allEntries, 
-                length
-            }
-        )
-    );
+    res.status(200).json({
+        data: allEntries,
+        length,
+    })
 }
 
 export default allAvos;

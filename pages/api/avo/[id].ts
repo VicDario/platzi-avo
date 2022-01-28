49c0c84ee0b2506cce515462257de593301bd736
Victor Fernandez <vfernandez@dcanje.com>
2022-01-28 16:12:58 -0300

    Cors added

--- pages/api/avo/[id].ts
+++ pages/api/avo/[id].ts
@@ -1,11 +1,18 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import DB from '@database';
+import enablePublicAccess from '@cors';
 
-const avoById = async(req: NextApiRequest, res: NextApiResponse) => {
-    const db = new DB();
-    const { id } = req.query;
-    const avo = await db.getById(id as string);
-    res.status(200).json(avo);
-}
+const avoById = async (req: NextApiRequest, res: NextApiResponse) => {
+	try {
+		await enablePublicAccess(req, res);
+		const db = new DB();
+		const { id } = req.query;
+		const avo = await db.getById(id as string);
+		res.status(200).json(avo);
+	} catch (e) {
+		console.error(e);
+		res.status(500).json({ length: 0, data: [], error: 'Something went wrong' });
+	}
+};
 
 export default avoById;
--- pages/api/avo/index.ts
+++ pages/api/avo/index.ts
@@ -1,14 +1,25 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import DB from '@database';
+import enablePublicAccess from '@cors';
 
-const allAvos = async(req: NextApiRequest, res: NextApiResponse) => {
-    const db = new DB();
-    const allEntries = await db.getAll();
-    const length = allEntries.length;
-    res.status(200).json({
-        data: allEntries,
-        length,
-    })
-}
+const allAvos = async (req: NextApiRequest, res: NextApiResponse) => {
+	try {
+		await enablePublicAccess(req, res);
+		const db = new DB();
+		const allEntries = await db.getAll();
+		const length = allEntries.length;
+		res.status(200).json({
+			data: allEntries,
+			length,
+		});
+	} catch (e) {
+		console.error(e);
+		res.status(500).json({
+			length: 0,
+			data: [],
+			error: 'Something went wrong',
+		});
+	}
+};
 
 export default allAvos;
